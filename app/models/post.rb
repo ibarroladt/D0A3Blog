b86d207c8e929128ea870ad4b51e8d6ab90f24d4
sabefo <santiagofortes@gmail.com>
2016-01-04 13:55:38 -0600

    before save for post attrs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,22 @@
 class Post < ActiveRecord::Base
 
   validates_presence_of :title, :summary, :photo, :content
+  before_save :titleize_title, :capitalize_summary, :capitalize_content
 
   # TODO falta meter detalles de la foto
+
+  private
+
+    def titleize_title
+      self.send("#{:title}=", self.send(:title).titleize) if self.send(:title)
+    end
+
+    def capitalize_summary
+      self.send("#{:summary}=", self.send(:summary).capitalize) if self.send(:summary)
+    end
+
+    def capitalize_content
+      self.send("#{:content}=", self.send(:content).capitalize) if self.send(:content)
+    end
 
 end
