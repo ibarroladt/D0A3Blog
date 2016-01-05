09be0a8a8612b78e111d1963e272d2c89ed2ccfb
sabefo <santiagofortes@gmail.com>
2016-01-05 16:53:26 -0600

    added all methods to category controller

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,16 @@
 class Category < ActiveRecord::Base
 
+  # Validations
   validates_presence_of :name
   before_save :titleize_name
 
+  # Relations
   has_many :posts_in_categories
   has_many :posts, through: :posts_in_categories
+
+  # Readable url
+  extend FriendlyId
+  friendly_id :name
 
   def titleize_name
     self.send("#{:name}=", self.send(:name).titleize) if self.send(:name)
