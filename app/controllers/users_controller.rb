b16a4e040f0876ef082d415d1ffa892fc9043620
sabefo <santiagofortes@gmail.com>
2016-02-25 10:54:33 -0600

    created users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
   get "/posts/unpublished", :to => "posts#unpublished", :as => 'unpublished'
   root 'static_pages#home'
   resources :posts
