b8c8d11699a39b65166ab71b410bfebc2722913b
sabefo <santiagofortes@gmail.com>
2016-02-29 16:52:17 -0600

    users edit done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Actualización exitosa"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:email, :password)
