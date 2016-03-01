bca8d562e6c644b2e94cbca958e06707510dfa85
sabefo <santiagofortes@gmail.com>
2016-03-01 13:11:32 -0600

    current user methods in users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+# To know who is the current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:id])
+  end
+
+  # Checks if the user is the current user, boolean
+  def current_user?(user)
+    user == current_user
+  end
+
 end
