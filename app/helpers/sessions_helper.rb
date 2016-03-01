15b8b943774e2d60ec2db7e59fc5756ab76dbb72
sabefo <santiagofortes@gmail.com>
2016-03-01 13:42:22 -0600

    moved methods from users helper to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  # To know who is the current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:id])
+  end
+
+  # Checks if the user is the current user, boolean
+  def current_user?(user)
+    user == current_user
+  end
+
+  # To know if user is logged in or not
+  def signed_in?
+    !current_user.nil?
+  end
+  
 end
