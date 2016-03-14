467daa7d41f512216b162cba38cdc58e59520849
sabefo <santiagofortes@gmail.com>
2016-03-14 09:26:57 -0600

    added config.assets.paths to application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module D0A3Blog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
