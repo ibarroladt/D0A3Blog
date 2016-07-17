c4fb1772832422df0eeae08adf38ef42c7f776e7
sabefo <santiagofortes@gmail.com>
2016-07-16 21:49:52 -0500

    .

--- db/migrate/20160104183619_add_new_attrs.rb
+++ db/migrate/20160104183619_add_new_attrs.rb
@@ -6,7 +6,6 @@ class AddNewAttrs < ActiveRecord::Migration
     add_column    :posts, :content, :text
     add_column    :posts, :publish, :boolean
     add_column    :posts, :summary, :string
-    add_column    :posts, :visits_count, :integer, default: 0
   end
 
 end
